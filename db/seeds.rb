c3c21fa6ec3a5d93536148713aa3f321ba26b479
Andrej Antas <andrej.antas@gmail.com>
2013-10-10 08:33:21 +0200

    some basic sacffolding done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:'ME', phone:'777701701', email:'me@me.com', age:'45')
+User.create(name:'you', phone:'701701701', email:'you@me.com', age:'25')
+User.create(name:'he', phone:'650701701', email:'he@me.com', age:'27')
+User.create(name:'she', phone:'765701701', email:'she@me.com', age:'32')
+User.create(name:'it', phone:'432701701', email:'it@me.com', age:'44')
